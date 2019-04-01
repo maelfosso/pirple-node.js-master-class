3fe04f5e23e03b982805d8cfda778fe422fee43a
Mael FOSSO <fosso.mael.elvis@gmail.com>
2019-04-01 18:48:03 +0100

    Routing Requests

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var server = http.createServer(function(req, res) {
   var parseUrl = url.parse(req.url, true);
 
   // Get the path
-  var path = parseUrl.path;
+  var path = parseUrl.pathname;
   var trimmedPath = path.replace(/^\/+|\/+$/g,'')
 
   // Get the query String as an object
@@ -27,14 +27,44 @@ var server = http.createServer(function(req, res) {
   req.on('data', function(data) {
     buffer += decoder.write(data);
   });
-  req.on('end', function(data) {
+  req.on('end', function() {
     buffer += decoder.end();
+
+    // Choose the handler this request should go to
+    var choosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+    // Construct the data object to send to the handler
+    var data = {
+      'trimmedPath': trimmedPath,
+      'queryStringObject': queryStringObject,
+      'method': method,
+      'headers': headers,
+      'payload': buffer
+    };
+    
+    // Route the request to the choosen handler specified in the router
+    choosenHandler(data, function(statusCode, payload) {
+      // Use the status code call back by the handler or default to 200
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+      // Use a default payload callback by the handler or define an empty object
+      payload = typeof(payload) == 'object' ? payload : {};
+
+      // Convert the payload to a string
+      var payloadString = JSON.stringify(payload);
+
+      // Return the response
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      // Log the path
+      console.log('Returning this response ', statusCode, payloadString);
+    })
 
     // Send the reponse
     res.end('Hello World !\n');
 
-    // Log the path
-    console.log('Request received with these payload: ', buffer);
+
   });
 
 });
@@ -43,3 +73,23 @@ var server = http.createServer(function(req, res) {
 server.listen(3000, function() {
   console.log('The server is listening on port 3000 now');
 })
+
+// Define  Handlers for router
+var handlers = {};
+
+// Sample Handler
+handlers.sample = function(data, callback) {
+  // Callback a http status code and a payload object
+
+  callback(406, {'name': 'sample handler'});
+}
+
+// Not found handler
+handlers.notFound = function(data, callback) {
+  callback(404);
+}
+
+// Define a request router
+var router = {
+  'sample': handlers.sample
+}
